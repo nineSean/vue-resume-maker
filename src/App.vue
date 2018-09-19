6e18d25247d98be7f0540b44e10299cc3a9a9a40
nineSean <ninesean1989@gmail.com>
2018-09-19 19:53:59 +0800

    create single-file components and use flex-box layout

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <header>
+      <Topbar/>
+    </header>
+    <main>
+      <ResumeEditor/>
+      <ResumePreview/>
+    </main>
+  </div>
+</template>
+
+<script>
+  import "./assets/reset.css";
+  import "normalize.css/normalize.css";
+
+  import Topbar from "./components/Topbar";
+  import ResumePreview from "./components/ResumePreview";
+  import ResumeEditor from "./components/ResumeEditor";
+
+  export default {
+    name: "app",
+    data() {
+      return {
+        text: "welcome"
+      };
+    },
+    components: { Topbar, ResumePreview, ResumeEditor }
+  }
+</script>
+
+<style>
+  body {
+    background-color: #545;
+
+    font: 20px/1.5 'STHeitiSC-Light', sans-serif;
+    display: flex;
+    justify-content: space-around;
+  }
+  #app{
+    flex-grow: 1;
+    max-width: 1440px;
+    min-width: 1024px;
+    height: 100vh;
+    padding-bottom: 12px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  #app>header{
+    width: 100%;
+  }
+  #app>main{
+    width: 100%;
+    flex-grow: 1;
+    display: flex;
+    justify-content: space-around;
+  }
+</style>
---
+++ src/components/topbar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id ="topbar">
+    <h1>简 历</h1>
+    <button>保存</button>
+    <button>预览</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Topbar"
+};
+</script>
+
+<style>
+#topbar {
+  width: 100%;
+  height: 64px;
+  margin-bottom: 12px;
+  padding: 16px 18px;
+  display: flex;
+  align-items: center;
+  background: #ddd;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.25);
+}
+#topbar > h1 {
+  flex-grow: 1;
+  font-size: 1.5em;
+}
+#topbar > button {
+  width: 72px;
+  height: 32px;
+  margin-left: 16px;
+}
+#topbar > button:hover {
+  box-shadow: 1px 1px 1px rgba(0, 0, 0, .2);
+}
+#topbar > button:nth-child(1) {
+  color: #fff;
+  background: #02af5f;
+}
+#topbar > button:nth-child(2) {
+  color: #222;
+  background: #aaa;
+}
+</style>
+
+
