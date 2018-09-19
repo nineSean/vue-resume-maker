ba715487f2d1d625b5ac41d12f41eaa530f9157d
nineSean <ninesean1989@gmail.com>
2018-09-19 19:53:59 +0800

    create single-file components and use flex-box layout

---
+++ src/components/ResumeEditor.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="resumeEditor">
+    resumeEditor
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ResumeEditor'
+  }
+</script>
+
+<style>
+  #resumeEditor{
+    background-color: #fff;
+
+    width: 35%;
+  }
+</style>
+
---
+++ src/components/ResumePreview.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="resumePreview">
+    preview
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'ResumePreview'
+  }
+</script>
+
+<style>
+  #resumePreview{
+    background-color: #fff;
+
+    width: 62.5%;
+  }
+</style>
+
